ea476052a30d42f8354d0b1526067048e5feabfd
Debbie-Stack <deboraheffiong202@gmail.com>
2026-03-24 23:13:40 +0100

    fixed the nav button for mobile view

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const slide = document.getElementById("slide")
 
 function showMenu(){
   slide.classList.toggle("slide")
+} 
+
+const desktopSlide = document.getElementById("desktopSlide")
+
+function showMenu(){
+  slide.classList.toggle("desktopSlide")
 }
