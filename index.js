5276d2a75ac1fcd14f75adec5baa45bb4a2c223a
Debbie-Stack <deboraheffiong202@gmail.com>
2026-04-12 18:34:28 +0100

    working on mobile view bg-image

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
-const slide = document.getElementById("slide")
+const displayProject = document.getElementById("portfolio");
 
-function showMenu(){
-  slide.classList.toggle("slide")
-} 
+const projects = [
+  {
+    image: "./image/PROJECT 1 IMAGE.png",
+    name: "FLAVORIZ",
+    technology: ["React.js", "TailwindCss"],
+    github: "https://github.com/Debbie-Stack/JavaScript",
+    live: "http://www.google.com",
+  },
 
-// const desktopSlide = document.getElementById("desktopSlide")
+  {
+    image: "./image/PROJECT 2 IMAGE.png",
+    name: "BIGTRYBE",
+    technology: ["React.js", "TailwindCss"],
+    github: "https://github.com/Debbie-Stack/JavaScript",
+    live: "http://127.0.0.1:5500/index.html",
+  },
 
-// function showMenu(){
-  // slide.classList.toggle("desktopSlide")
-// }
+  {
+    image: "./image/PROJECT 1 IMAGE.png",
+    name: "FLAVORIZ",
+    technology: ["React.js", "TailwindCss"],
+    github: "https://github.com/Debbie-Stack/JavaScript",
+    live: "http://127.0.0.1:5500/index.html",
+  },
+];
+
+// Aside bar function
+const slide = document.getElementById("slide");
+function showMenu() {
+  slide.classList.toggle("slide");
+}
+
+// Skills Display Function
+const projectDisplayFunction = () => {
+  projects.map((project, index) => {
+    return displayProject.innerHTML += `<div class="bg-pink-300/60 h-90 rounded-lg backdrop-blur-3xl">
+            <div class="project-1">
+                <img src="${project.image}" class="w-100 rounded-t-lg">
+              </div>
+              <div>
+              <div class="p-3 font-bold text-xl">
+                <h1>${project.name}</h1>  
+              </div>
+              <div id="main-div" class="flex justify-between items-center p-3">
+                <div>
+                <button class="bg-purple-700 text-white py-1 px-3 rounded-full">React.Js</button> 
+                <button class="bg-white text-purple-900 py-1 px-3 rounded-full">TailwindCss</button>              
+              </div>
+              <div>
+              <button><a href=${project.github} target="_blank"><i class="fa-brands fa-square-github text-4xl text-purple-600"></i></a></button>
+              <button><a href=${project.live}><i class="fa-regular fa-eye text-white"></i></a></button>
+              </div>
+              </div>
+              </div>
+            </div>`;
+  });
+};
+
+projectDisplayFunction();
+
+
---
+++ projectData.js
@@ -0,0 +1,23 @@
+[
+    {
+        image: "./image/PROJECT 1 IMAGE.png",
+        name: "Flavoriz",
+        technology: ["html", "tailwindCss", "javaScript"],
+        github: "https://github.com/Debbie-Stack/JavaScript"
+    },
+
+     {
+        image: "./image/PROJECT 1 IMAGE.png",
+        name: "Flavoriz",
+        technology: ["html", "tailwindCss", "javaScript"],
+        github: "https://github.com/Debbie-Stack/JavaScript"
+    },
+
+     {
+        image: "./image/PROJECT 1 IMAGE.png",
+        name: "Flavoriz",
+        technology: ["html", "tailwindCss", "javaScript"],
+        github: "https://github.com/Debbie-Stack/JavaScript"
+    },
+
+]
